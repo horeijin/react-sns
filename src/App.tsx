a10396c4f8b16de0e6bcba1b5a23d16d44836f05
myMac <dgbuzzer@gmail.com>
2023-12-11 17:00:00 +0900

    feature/2-1: firebaseAuth 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import { Router } from "components/Router";
 import { Layout } from "components/Layout";
 
+import { getAuth } from "firebase/auth";
+import { app } from "firebaseApp";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
+  const auth = getAuth(app);
+
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  );
+
   return (
     <Layout>
+      <ToastContainer
+        theme="dark"
+        autoClose={1000}
+        hideProgressBar
+        newestOnTop
+      />
       <Router />
     </Layout>
   );
