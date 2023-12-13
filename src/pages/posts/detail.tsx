886ef2b0e370dac5997b6165009bf248b14e9e2a
myMac <dgbuzzer@gmail.com>
2023-12-13 11:44:18 +0900

    feature/7-1: add comments

--- src/pages/posts/detail.tsx
+++ src/pages/posts/detail.tsx
@@ -6,8 +6,10 @@ import Loader from "components/loader/Loader";
 import { PostBox } from "components/posts/PostBox";
 
 import { db } from "firebaseApp";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
 import { PostHeader } from "components/posts/PostHeader";
+import { CommentForm } from "components/comments/CommentForm";
+import { CommentBox, CommentProps } from "components/comments/CommentBox";
 
 interface Props {}
 
@@ -18,9 +20,9 @@ export const PostDetail: FC<Props> = ({}) => {
   const getPost = useCallback(async () => {
     if (params.id) {
       const docRef = doc(db, "posts", params.id);
-      const docSnap = await getDoc(docRef);
-
-      setPost({ ...(docSnap?.data() as PostProps), id: docSnap?.id });
+      onSnapshot(docRef, (doc) => {
+        setPost({ ...(doc?.data() as PostProps), id: doc.id });
+      });
     }
   }, [params.id]);
 
@@ -31,7 +33,20 @@ export const PostDetail: FC<Props> = ({}) => {
   return (
     <div className="post">
       <PostHeader />
-      {post ? <PostBox post={post} /> : <Loader />}
+      {post ? (
+        <>
+          <PostBox post={post} />
+          <CommentForm post={post} />
+          {post?.comments
+            ?.slice(0)
+            ?.reverse()
+            ?.map((data: CommentProps, index: number) => (
+              <CommentBox data={data} key={index} post={post} />
+            ))}
+        </>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 };
