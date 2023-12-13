f21ae4b4d75756d8c47c824de34268d1aaf0e329
myMac <dgbuzzer@gmail.com>
2023-12-13 10:04:12 +0900

    feature/5-3: profile image upload & edit

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,7 +1,85 @@
-import { FC } from "react";
+import { FC, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "context/AuthContext";
+
+import { PostProps } from "pages/home";
+import { PostBox } from "components/posts/PostBox";
+
+import { db } from "firebaseApp";
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+
+const PROFILE_DEFAULT_URL = "/logo512.png";
 
 interface Props {}
 
 export const ProfilePage: FC<Props> = ({}) => {
-  return <>프로필 페이지</>;
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (user) {
+      let postsRef = collection(db, "posts");
+      let postsQuery = query(
+        postsRef,
+        where("uid", "==", user.uid),
+        orderBy("createdAt", "desc")
+      );
+
+      onSnapshot(postsQuery, (snapShot) => {
+        let dataObj = snapShot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc?.id,
+        }));
+        setPosts(dataObj as PostProps[]);
+      });
+    }
+  }, [user]);
+
+  return (
+    <div className="home">
+      <div className="home__top">
+        <div className="home__title">Profile</div>
+        <div className="profile">
+          <img
+            src={user?.photoURL || PROFILE_DEFAULT_URL}
+            alt="profile"
+            className="profile__image"
+            width={100}
+            height={100}
+          />
+          <button
+            type="button"
+            className="profile__btn"
+            onClick={() => navigate("/profile/edit")}
+          >
+            Profile Edit
+          </button>
+        </div>
+        <div className="profile__text">
+          <div className="profile__name">{user?.displayName || "사용자님"}</div>
+          <div className="profile__email">{user?.email}</div>
+        </div>
+        <div className="home__tabs">
+          <div className="home__tab home__tab--active">For You</div>
+          <div className="home__tab">Likes</div>
+        </div>
+        <div className="post">
+          {posts?.length > 0 ? (
+            posts?.map((post) => <PostBox post={post} key={post.id} />)
+          ) : (
+            <div className="post__no-posts">
+              <div className="post__text">게시글이 없습니다.</div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
